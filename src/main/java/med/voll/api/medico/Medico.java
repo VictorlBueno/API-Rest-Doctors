c06da1ffb74928302d8fb110839426764c77c700
Victor <vihbuenoz@gmail.com>
2023-09-24 10:00:01 -0300

    first commit

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -21,19 +21,36 @@ public class Medico {
     private String telefone;
     private String email;
     private String crm;
-
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
-
     @Embedded // diz que tem classes diferentes, mas que fiquem na mesma tabela
     private Endereco endereco;
+    private boolean ativo;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.crm = dados.crm();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null) {
+            this.endereco.atualizaInformacoes(dados.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
